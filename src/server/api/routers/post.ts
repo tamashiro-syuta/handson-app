e7915931e452210e0706678500adca0bef17ba79
tamashiro-syuta <syuta5825@gmail.com>
2023-10-18 00:10:16 +0900

    ✨ 一覧ページを作成

--- src/server/api/routers/post.ts
+++ src/server/api/routers/post.ts
@@ -1,9 +1,11 @@
 import { z } from "zod";
 
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { db } from "~/server/db";
 
 export const postRouter = createTRPCRouter({
   hello: publicProcedure
+    // textはstring型で必須に設定
     .input(z.object({ text: z.string() }))
     .query(({ input }) => {
       return {
@@ -11,15 +13,21 @@ export const postRouter = createTRPCRouter({
       };
     }),
 
-  create: publicProcedure
-    .input(z.object({ name: z.string().min(1) }))
-    .mutation(async ({ ctx, input }) => {
+  getAllBlogs: publicProcedure.query(() => {
+    return db.post.findMany();
+  }),
+
+  postBlog: publicProcedure
+    .input(z.object({ title: z.string().min(1), description: z.string() }))
+    // NOTE: mutationは更新系(create or update or delete)の時に使う
+    .mutation(async ({ input }) => {
       // simulate a slow db call
       await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      return ctx.db.post.create({
+      return db.post.create({
         data: {
-          name: input.name,
+          title: input.title,
+          description: input.description,
         },
       });
     }),
